ece66c92bc3004ec08a21cb8d80c70d5683a4579
gladishevvasiliy <gladishevvasiliy@gmail.com>
2016-03-17 11:47:20 +0300

    displayArray working

--- 2D_array/2D_array.cpp
+++ 2D_array/2D_array.cpp
@@ -14,6 +14,7 @@
 
 using namespace std;
 
+void displayArray(int a[][3], int n);
 
 int main()
 {
@@ -23,19 +24,14 @@ int main()
 					{ 4,5,6 },
 					{ 7,8,9 } };
 	printf_s("elements of array: ");
+	/**/
+	displayArray(arr, N);
 	/*for (int i = 0; i < N; i++) {
-	for (int j = 0; j < N; j++) {
-	cin >> arr[i][j];
-	cout << " ";
-	}
-	}*/
-
-	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			cout << arr[i][j] << " ";
 		}
 	}
-	cout << endl;
+	cout << endl;*/
 
 	//cумму эл-тов в тех строках, которые не содержат отрицательных эл-тов
 	int s = 0;
@@ -71,7 +67,7 @@ int main()
 		cout << arrSum[i] << " ";
 	cout << endl;
 
-	int minSumArr = arrSum[0];
+	float minSumArr = arrSum[0];
 	for (int i = 1; i < m; i++)
 		if (arrSum[i] < minSumArr) minSumArr = arrSum[i];
 
@@ -81,4 +77,11 @@ int main()
 	return 0;
 }
 
-
+void displayArray(int a[][3], int n) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << a[i][j] << " ";
+		}
+	}
+	cout << endl;
+}
