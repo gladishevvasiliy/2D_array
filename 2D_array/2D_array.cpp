a0a9caf24907eb24fee047ea40a7ffebec2a1bd3
gladishevvasiliy <gladishevvasiliy@gmail.com>
2016-03-21 16:02:10 +0300

    addad no-func code

--- 2D_array/2D_array.cpp
+++ 2D_array/2D_array.cpp
@@ -8,58 +8,36 @@
 #include <cmath>
 #include <clocale>
 #include <cfloat>
-#include <iostream> 
+#include <iostream>
 #include <conio.h>
 
 
 using namespace std;
 
-int sumArray(int arr[][3], int N);
-int minSumArr(int arrSum[], int N);
-void displayArray1D(int a[], int n);
-void displayArray2D(int a[][3], int n);
-void sumDiangle(int arr[][3], int arrSum[], int N, int M);
-
 
 int main()
 {
-	int const N = 3; 
-	int arr[N][N] = { { 2,-2,3 },
-					{ -4,4,6 },
-					{ 7,-23,9 } };
-	////////////////////////// OUTPUT ARR //////////////////////////
+
+	int const N = 3;
+	int arr[N][N] = { { 1,2,3 },
+	{ 4,5,6 },
+	{ 7,8,9 } };
 	printf_s("elements of array: ");
-	displayArray2D(arr, N);
-	//////////////////////// SUM POSITIVE ROWS ////////////////////
-	cout << "sum of the elemetns from rows that don't include negative numbers = " << sumArray(arr, N) << endl;
-	int arrSum[(N * 2) - 1] = { 0 };
-	/////////////////////// SUM DIANGLE //////////////////////////
-	sumDiangle(arr, arrSum, N, (N * 2) - 1);
-	cout << "sumArr = ";
-	displayArray1D(arrSum, (N * 2) - 1);
-	////////////////////// MIN SUM DIANGLE //////////////////////
-	cout << "minSumArr = " << minSumArr(arrSum, N) << endl;
+	/*for (int i = 0; i < N; i++) {
+	for (int j = 0; j < N; j++) {
+	cin >> arr[i][j];
+	cout << " ";
+	}
+	}*/
 
-	system("pause");
-	return 0;
-}
-
-void displayArray2D(int a[][3], int n) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cout << a[i][j] << " ";
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << arr[i][j] << " ";
 		}
 	}
 	cout << endl;
-}
 
-void displayArray1D(int a[], int n) {
-	for (int i = 0; i < n; i++)
-		cout << a[i] << " ";
-	cout << endl;
-}
 
-int sumArray(int arr[][3], int N) {
 	int s = 0;
 	int s1;
 	for (int i = 0; i < N; i++) {
@@ -71,20 +49,129 @@ int sumArray(int arr[][3], int N) {
 		s += s1;
 
 	}
-	return s;
-}
+	cout << "sum of the elemetns from rows that don't include negative numbers = " << s << endl;
 
-void sumDiangle(int arr[][3], int arrSum[], int N, int M) {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
+	int n = 3;
+	int m = (n * 2) - 1;
+	float *arrSum = new float[m];
+
+	//int tmp = (n * 2) - 1;
+	//int const m = tmp;
+	//float arrSum[N*2-1];
+	for (int i = 0;i < m; i++) arrSum[i] = 0;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
 			arrSum[i + j] = arrSum[i + j] + arr[i][j];
 		}
 	}
+
+	cout << "sumArr = ";
+	for (int i = 0; i < m; i++)
+		cout << arrSum[i] << " ";
+	cout << endl;
+
+	int minSumArr = arrSum[0];
+	for (int i = 1; i < m; i++)
+		if (arrSum[i] < minSumArr) minSumArr = arrSum[i];
+
+	cout << "minSumArr = " << minSumArr << endl;
+
+	system("pause");
+	return 0;
 }
 
-int minSumArr(int arrSum[], int N) {
-	int minSum = arrSum[0];
-	for (int i = 1; i < (N * 2) - 1; i++)
-		if (arrSum[i] < minSum) minSum = arrSum[i];
-	return minSum;
-}
+
+
+
+
+///// 2d-array.cpp: определяет точку входа для консольного приложения.
+////cумму эл-тов в тех строках, которые не содержат отрицательных эл-тов
+////макс среди сумм эл-тов диагоналей, паралельной главной
+//
+//#include "stdafx.h"
+//#include <cstdlib>
+//#include <cstdio>
+//#include <cmath>
+//#include <clocale>
+//#include <cfloat>
+//#include <iostream> 
+//#include <conio.h>
+//
+//
+//using namespace std;
+//
+//int sumArray(int arr[][3], int N);
+//int minSumArr(int arrSum[], int N);
+//void displayArray1D(int a[], int n);
+//void displayArray2D(int a[][3], int n);
+//void sumDiangle(int arr[][3], int arrSum[], int N, int M);
+//
+//
+//int main()
+//{
+//	int const N = 3; 
+//	int arr[N][N] = { { 2,-2,3 },
+//					{ -4,4,6 },
+//					{ 7,-23,9 } };
+//	////////////////////////// OUTPUT ARR //////////////////////////
+//	printf_s("elements of array: ");
+//	displayArray2D(arr, N);
+//	//////////////////////// SUM POSITIVE ROWS ////////////////////
+//	cout << "sum of the elemetns from rows that don't include negative numbers = " << sumArray(arr, N) << endl;
+//	int arrSum[(N * 2) - 1] = { 0 };
+//	/////////////////////// SUM DIANGLE //////////////////////////
+//	sumDiangle(arr, arrSum, N, (N * 2) - 1);
+//	cout << "sumArr = ";
+//	displayArray1D(arrSum, (N * 2) - 1);
+//	////////////////////// MIN SUM DIANGLE //////////////////////
+//	cout << "minSumArr = " << minSumArr(arrSum, N) << endl;
+//
+//	system("pause");
+//	return 0;
+//}
+//
+//void displayArray2D(int a[][3], int n) {
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < n; j++) {
+//			cout << a[i][j] << " ";
+//		}
+//	}
+//	cout << endl;
+//}
+//
+//void displayArray1D(int a[], int n) {
+//	for (int i = 0; i < n; i++)
+//		cout << a[i] << " ";
+//	cout << endl;
+//}
+//
+//int sumArray(int arr[][3], int N) {
+//	int s = 0;
+//	int s1;
+//	for (int i = 0; i < N; i++) {
+//		s1 = 0;
+//		for (int j = 0; j < N; j++) {
+//			if (arr[i][j] > 0) s1 = s1 + arr[i][j];
+//			else { s1 = 0; break; };
+//		}
+//		s += s1;
+//
+//	}
+//	return s;
+//}
+//
+//void sumDiangle(int arr[][3], int arrSum[], int N, int M) {
+//	for (int i = 0; i < N; i++) {
+//		for (int j = 0; j < N; j++) {
+//			arrSum[i + j] = arrSum[i + j] + arr[i][j];
+//		}
+//	}
+//}
+//
+//int minSumArr(int arrSum[], int N) {
+//	int minSum = arrSum[0];
+//	for (int i = 1; i < (N * 2) - 1; i++)
+//		if (arrSum[i] < minSum) minSum = arrSum[i];
+//	return minSum;
+//}
